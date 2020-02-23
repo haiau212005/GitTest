9aaedc144e5000fc6fe6b0f241227c04dbca9b83
Hải Anh Trần <tranhaianh21021989@gmail.com>
2020-02-23 09:16:08 +0700

    okokok

--- TestGit/Program.cs
+++ TestGit/Program.cs
@@ -17,6 +17,7 @@ namespace TestGit
             Console.WriteLine("I'm haiau212007");
             Console.WriteLine("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee");
             Console.WriteLine("AnhTH");
+            Console.WriteLine("ahihihihi");
             Console.ReadKey();
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
