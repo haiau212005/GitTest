1ca6ab282cf8224e398455910c2055a57d18fe27
HienPM <hienpm@nal.vn>
2026-04-15 16:06:53 +0700

    cm 1

--- WebApplication1/Controllers/AdminController.cs
+++ WebApplication1/Controllers/AdminController.cs
@@ -11,6 +11,7 @@ namespace WebApplication1.Controllers
         // GET: Admin
         public ActionResult Index()
         {
+            var rs = "hehe";
             return View();
         }
     }
